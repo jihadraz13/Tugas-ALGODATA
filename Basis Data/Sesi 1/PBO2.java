b0885adb82f4f01140889fb5db0dc9c639dd855f
jihadraz13 <80213764+jihadraz13@users.noreply.github.com>
2021-09-27 10:15:00 +0700

    Add files via upload

---
+++ Basis Data/Sesi 1/PBO2.java
@@ -0,0 +1,13 @@
+
+package pbo2;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author salman alfariji
+ */
+public class PBO2 {
+    public static void main (String[]args){
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+}
