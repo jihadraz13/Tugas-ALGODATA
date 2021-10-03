ad9a7a2e2a858e45c67f9831b779bb58e2e571ed
jihadraz13 <80213764+jihadraz13@users.noreply.github.com>
2021-10-04 02:01:42 +0700

    Add files via upload

---
+++ PBO/Sesi 2/VariabelData.java
@@ -0,0 +1,19 @@
+package variabeldata;
+
+/**
+ *
+ * @author jihad
+ */
+public class VariabelData {
+    
+    
+    public static void main(String[]args){
+
+        int uanganak = 50000;
+        int hargamainan = 55000;
+        
+        String result = uanganak > hargamainan ? "Membeli":"Tidak Membeli";
+        
+        System.out.println(result);
+    }    
+}
